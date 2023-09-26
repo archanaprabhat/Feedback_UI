1c2472f641f193ad1be744869e1bd64a8371c5a4
ArchanaPrabhath <archanaprabhath@vighneshs-MacBook-Air.local>
2023-09-26 22:15:07 +0530

    review

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
+
+
 function App() {
+    const [feedback,setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
     return (
         <>
         <Header />
-         <div className="conatiner">
-        <FeedbackItem />
+         <div className="container">
+            <FeedbackForm />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback} 
+            handleDelete={deleteFeedback} />
         </div>
         </>
     )
 }
-
+ 
 export default App
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,23 @@
-import {useState} from 'react'
-function FeedbackItem() {
-    const[rating, setRating] = useState(7)
-    const[text, setText] = useState('This is an example of a feedback item')
-    
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev)
-            return prev + 1
-        })
-    }
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
+function FeedbackItem({item, handleDelete }) { 
   return (
-    <div className="card">
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}
-            <button onClick={handleClick}>Click</button>
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+            <FaTimes color = 'purple' />
+        </button>
+        <div className="text-display">{item.text}
         </div>
 
-    </div>
+    </Card>
   )
+}
+
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
