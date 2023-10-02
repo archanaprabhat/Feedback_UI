14671fbb2a0c35d7f78e87f8d240fab3c4fcfe9d
ArchanaPrabhath <archanaprabhath@vighneshs-MacBook-Air.local>
2023-10-02 18:17:56 +0530

    icon

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
 import Card from "../components/shared/Card"
-
+import {Link} from 'react-router-dom'
 function AboutPage() {
   return (
    <Card>
@@ -8,7 +8,7 @@ function AboutPage() {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quibusdam.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, voluptate nisi quod amet eius fugit atque. Beatae amet rem, nam culpa doloribus saepe odit sed, hic, nisi quibusdam minima qui.</p>
         <p>
-                <a href="/">Back to Home</a>
+                <Link to="/">Back to Home</Link>
         </p>
     </div>
     </Card>
