5a62e985fc2f28420ae26cb99ee26f66091fea48
ArchanaPrabhath <archanaprabhath@vighneshs-MacBook-Air.local>
2023-10-02 15:19:34 +0530

    About

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,41 +1,54 @@
-import { useState } from "react"
-import RatingSelect from "./RatingSelect"
-import Card from "./shared/Card"
-import Button from "./shared/Button"
+import { useState } from "react";
+import RatingSelect from "./RatingSelect";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
 
-function FeedbackForm() {
-    const [text,setText] = useState('')
-    const [Rating,setRating] = useState(10)
-    const [btnDisabled,setBtnDisabled] = useState('true')
-    const [message,setMessage] = useState('')
-    const handleTextChange = ({ target: { value } }) => {
-        if (value === '') {
-          setBtnDisabled(true)
-          setMessage(null)
-          
-        } else if (value.trim().length < 10) { 
-          setMessage('Text must be at least 10 characters')
-          setBtnDisabled(true)
-        } else {
-          setMessage(null)
-          setBtnDisabled(false)
-        }
-        setText(value)
+function FeedbackForm({handleAdd}) {
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 10) {
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
       }
-      
+      handleAdd(newFeedback)
+
+      setText('')
+    }
+  };
+
   return (
     <Card>
-        <form >
-            <h2>How would you rate your service with us?</h2>
-            <RatingSelect select={(rating) => setRating(rating)} />
-            <div className="input-group">
-                <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                <Button type="submit" isDisabled={btnDisabled} >Send </Button>
-            </div>
-            {message && <div className="message">{message}</div>}
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
+          <Button type="submit" isDisabled={btnDisabled}>Send </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
-  )
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+  return (
+   <Card>
+    <div className="about">
+        <h1>About this Project</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quibusdam.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, voluptate nisi quod amet eius fugit atque. Beatae amet rem, nam culpa doloribus saepe odit sed, hic, nisi quibusdam minima qui.</p>
+        <p>
+                <a href="/">Back to Home</a>
+        </p>
+    </div>
+    </Card>
+  )
+}
+
+export default AboutPage
